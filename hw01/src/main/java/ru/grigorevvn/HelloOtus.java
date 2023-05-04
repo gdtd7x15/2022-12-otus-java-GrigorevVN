360c57bdb1f827422f02c7eda049851f3cd37bf8
GrigorevVN <126937@gmail.com>
2023-05-04 21:00:39 +0300

    hw01-gradle

---
+++ hw01/src/main/java/ru/grigorevvn/HelloOtus.java
@@ -0,0 +1,9 @@
+package ru.grigorevvn;
+
+import com.google.common.base.Strings;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println(Strings.repeat("Hello Otus! \n", 10));
+    }
+}
