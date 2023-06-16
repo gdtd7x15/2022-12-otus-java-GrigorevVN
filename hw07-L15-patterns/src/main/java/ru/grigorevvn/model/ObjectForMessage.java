f17341d7a7d1b0c319e9e5925eaba8afcb125657
GrigorevVN <126937@gmail.com>
2023-06-16 05:25:47 +0300

    hw07-L15-patterns

--- hw07-L15-patterns/src/main/java/ru/grigorevvn/model/ObjectForMessage.java
+++ hw07-L15-patterns/src/main/java/ru/grigorevvn/model/ObjectForMessage.java
@@ -1,12 +1,13 @@
 package ru.grigorevvn.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ObjectForMessage {
     private List<String> data;
 
     public List<String> getData() {
-        return data;
+        return data != null ? data : Collections.emptyList();
     }
 
     public void setData(List<String> data) {
